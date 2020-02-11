f65e7b1b7ca2c19706f3ef985255e00fb99cc622
ahmed daoud <ahmed-daoud1@outlook.fr>
2020-02-11 16:09:21 +0100

    up

---
+++ GestionCours/Controllers/VIsitorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionCours.Controllers
+{
+    public class VIsitorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- GestionCours/Startup.cs
+++ GestionCours/Startup.cs
@@ -61,7 +61,7 @@ namespace GestionCours
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=VIsitor}/{action=Index}/{id?}");
             });
         }
     }
